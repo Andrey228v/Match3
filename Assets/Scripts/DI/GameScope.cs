c6124dea50b88356ae4a20dc3917b7a4b30d8fa9
Andrey <88499258+Andrey228v@users.noreply.github.com>
2026-02-18 23:05:04 +0300

    GridSystem

---
+++ Assets/Scripts/DI/GameScope.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Game.GridSystem;
+using VContainer;
+using VContainer.Unity;
+
+namespace Assets.Scripts.DI
+{
+    public class GameScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<Grid>(Lifetime.Singleton);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Game/Board/GameBoard.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Game.GridSystem;
+using Assets.Scripts.Game.Tiles;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Board
+{
+    public class GameBoard : MonoBehaviour
+    {
+
+        private readonly List<Tile> _tilesToRefill = new List<Tile>();
+
+        private GridSystem.Grid _grid;
+
+
+    }
+}
