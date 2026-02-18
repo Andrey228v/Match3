8777bee9a35be7cac7bf31c06b3039bf70b8d6e6
Andrey <88499258+Andrey228v@users.noreply.github.com>
2026-02-18 19:46:48 +0300

    Created TileScript
    Sprites

---
+++ Assets/Scripts/Game/Tiles/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Tiles
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        private SpriteRenderer _spriteRenderer;
+
+        public TileConfig TileConfig {  get; private set; }
+        public bool IsIntrectable { get; private set; }
+        public bool IsMatched { get; private set; }
+
+        public void SetTileConfig(TileConfig tileConfig)
+        {
+            TileConfig = tileConfig;
+            IsIntrectable = tileConfig.IsIntrectable;
+            IsMatched = false;
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.sprite = tileConfig.Sprite;
+        }
+
+        public bool SetMatch(bool value) => IsMatched = value;
+    }
+}
---
+++ Assets/Scripts/Game/Tiles/TileConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Tiles
+{
+    public enum TileKind
+    {
+        Normal,
+        Blank,
+        Jelly
+    }
+
+
+    [CreateAssetMenu(fileName = "TileConfig", menuName = "Config/TileConfig")]
+    public class TileConfig : ScriptableObject
+    {
+        [SerializeField] private Sprite _sprite;
+        [SerializeField] private TileKind _tileKind;
+        [SerializeField] private bool _isInterectable;
+
+        public Sprite Sprite { get => _sprite; private set => _sprite = value; }
+        public TileKind TileKind { get => _tileKind; private set => _tileKind = value; }
+        public bool IsIntrectable { get => _isInterectable; set => _isInterectable = value; }
+
+
+    }
+}
