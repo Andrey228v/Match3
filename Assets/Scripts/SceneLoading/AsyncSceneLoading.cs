c7190d5eee77ab47b850922ebed1a25b0cab8120
Andrey <88499258+Andrey228v@users.noreply.github.com>
2026-02-21 19:09:22 +0300

    Addresables

---
+++ Assets/Scripts/SceneLoading/AsyncSceneLoading.cs
@@ -0,0 +1,55 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.SceneLoading
+{
+    public class AsyncSceneLoading : IAsyncSceneLoading
+    {   
+        private readonly Dictionary<string, SceneInstance> _loaderScenes = new Dictionary<string, SceneInstance>();
+
+        private LoadingView _loadingView;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public AsyncSceneLoading(LoadingView loadingView)
+        {
+            _loadingView = loadingView;
+        }
+
+        public void LoadingIsDone(bool value)
+        {
+            _loadingView.SetActiveScreen(value != true);
+        }
+
+        public async UniTask LoadScene(string sceneName)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            LoadingIsDone(false);
+            await UniTask.Delay(TimeSpan.FromSeconds(2f), _cancellationTokenSource.IsCancellationRequested);
+            var loadedScene = await Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive).WithCancellation(_cancellationTokenSource.Token);
+            SceneManager.SetActiveScene(loadedScene.Scene);
+
+            if(_loaderScenes.ContainsKey(sceneName) == false)
+            {
+                _loaderScenes.Add(sceneName, loadedScene);
+            }
+
+            _cancellationTokenSource.Cancel();
+        }
+
+        public async UniTask UnLoadScene(string sceneName)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            var sceneInstance = _loaderScenes[sceneName];
+
+            await Addressables.UnloadSceneAsync(sceneInstance).WithCancellation(_cancellationTokenSource.Token).AsUniTask();
+            _loaderScenes.Remove(sceneName);
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoading/IAsyncSceneLoading.cs
@@ -0,0 +1,13 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Assets.Scripts.SceneLoading
+{
+    public interface IAsyncSceneLoading
+    {
+        public UniTask LoadScene(string sceneName);
+
+        public UniTask UnLoadScene(string sceneName);
+
+        public void LoadingIsDone(bool value);
+    }
+}
